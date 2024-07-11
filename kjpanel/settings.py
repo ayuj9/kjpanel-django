f345473faf98ec80f1254cdc8db1de98fc3431e6
ayuj9 <ayuj5571@gmail.com>
2024-07-11 20:33:16 +0530

    add external db

--- kjpanel/settings.py
+++ kjpanel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,15 +90,19 @@ WSGI_APPLICATION = 'kjpanel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'kjpanel',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ghar12345',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'kjpanel',
-        'USER': 'postgres',
-        'PASSWORD': 'ghar12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
--- kjpanel/urls.py
+++ kjpanel/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.urls import path , include
 from django.contrib import admin
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('auto/', include('autoS.urls')),
     path('admin/', admin.site.urls),
     path('kj/' , include('kjapp.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
