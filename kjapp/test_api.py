cf9b68bcff7fa39950080825abe2a25fcba5f8dd
ayuj9 <ayuj5571@gmail.com>
2024-09-16 14:56:36 +0530

    change

--- kjapp/test_api.py
+++ kjapp/test_api.py
@@ -1,18 +1,18 @@
-# import os 
+import os 
 
-# from django.urls import reverse
-# from rest_framework.test import APITestCase
-# from rest_framework import status
-# from .models import Client
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Client
 
-# class ClientTests(APITestCase):
-#     def test_create_client(self):
-#         url = reverse('client-list')  # URL for the ClientViewSet
-#         data = {'name': 'Test Client', 'email': 'test@example.com'}
-#         response = self.client.post(url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class ClientTests(APITestCase):
+    def test_create_client(self):
+        url = reverse('client-list')  # URL for the ClientViewSet
+        data = {'name': 'Test Client', 'email': 'test@example.com'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#     def test_get_clients(self):
-#         url = reverse('client-list')
-#         response = self.client.get(url, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_get_clients(self):
+        url = reverse('client-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
