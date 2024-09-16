c484540ddbff4c2f5d1cc938d60e77f6bda5bfb3
ayuj9 <ayuj5571@gmail.com>
2024-09-16 15:27:27 +0530

    ch

--- kjpanel/settings.py
+++ kjpanel/settings.py
@@ -70,6 +70,9 @@ CORS_ALLOWED_ORIGINS = [
 
 ALLOWED_HOSTS = ["*"]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
@@ -162,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
