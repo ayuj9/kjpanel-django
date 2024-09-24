0a78b5f6e5e78c31377f8ed417ee71cea15f0e12
ayuj9 <ayuj5571@gmail.com>
2024-09-24 15:48:18 +0530

    fix :url

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,50 +1,27 @@
 import pytest
 from django.urls import reverse
 import os
+import json
 import requests
 
+json_file_path = os.path.join(os.path.dirname(__file__), 'data.json')
 
 # BASE_URL = "https://panel-django.onrender.com/kj"
-
 BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000/kj")
 
-
+with open(json_file_path, 'r') as json_file:
+    data = json.load(json_file)
 
 def test_post_client_plan():
-    data =   {  "name": "lhyati ji ",
-        "age": 21,
-        "gender": "m",
-        "phone": "2423991513",
-        "email": "ayu112@gmail.com",
-        "diet_preference": "Vegetarian",
-        "diet_language": "hindi",
-       "note": "",
-        "address": {
-            "city": "gwl",
-            "state": "mp",
-            "country": "india",
-            "zone":"Asia/Kolkata"
-        },
-        "insights": [
-            {
-            "height": 5,
-            "current_weight": 50,
-            "target_weight": 60,
-            "height_Unit": "cm",
-            "weight_Unit": "kg",
-            "persona": "reduce wt"
+    person_details = data[0].get("details")
+    response = requests.post(BASE_URL  + "/clientAllDetail" , json=person_details)
+    assert response.status_code == 200 , f"POST request failed with status {response.status_code}"
+    response_get = requests.get(BASE_URL + "/clientAllDetail/1")
+    print("get data" ,response_get.json())
+    assert response.status_code == 200 , f"GET request failed with status {response.status_code}"
+    retrieved_data = response_get.json()
+    assert retrieved_data['phone'] == person_details['phone'] , f"Expected {person_details['name']} but got {retrieved_data['name']}"
 
-        }],
-        "plan": 
-           [ {
-                "plan_level": "B",
-                "status": "Created",
-                "duration": 33,
-                "start_time": "2024-08-23",
-                "end_time": "2024-12-23"
-            }]} 
-    response = requests.post(BASE_URL  + "/clientAllDetail" , json=data)
-    assert response.status_code == 200
 
 
 # def test_get_client_allDetail():
@@ -63,85 +40,16 @@ def test_post_client_plan():
 
 
 
-# def test_post_diet():
-#     data =    { "day1" : {
-#     "meal1": ["lemon"],
-#     "meal2": ["chekko"],
-#     "meal3": ["aloo"],
-#     "meal4": ["fff"],
-#     "meal5": ["lalu"],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#     "day2" :{
-#     "meal1": ["aloo"],
-#     "meal2": ["rice"],
-#     "meal3": [],
-#     "meal4": [],
-#     "meal5": [],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#     "day3" : {
-#     "meal1": [],
-#     "meal2": [],
-#     "meal3": [],
-#     "meal4": [],
-#     "meal5": [],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#     "day4" : {
-#     "meal1": [],
-#     "meal2": [],
-#     "meal3": [],
-#     "meal4": [],
-#     "meal5": [],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#     "day5" : {
-#     "meal1": [],
-#     "meal2": [],
-#     "meal3": [],
-#     "meal4": [],
-#     "meal5": [],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#     "day6" :{
-#     "meal1": [],
-#     "meal2": [],
-#     "meal3": [],
-#     "meal4": [],
-#     "meal5": [],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#     "day7" :{
-#     "meal1": [],
-#     "meal2": [],
-#     "meal3": [],
-#     "meal4": [],
-#     "meal5": [],
-#     "meal6": [],
-#     "meal7": []
-#   },
-#   "note":"",
-#   "meal_Time":{},
-#   "date":"2024-08-05",
-#   "client":2
-# }
-#     response = requests.post(BASE_URL + "/dietPlan" , json =data ) 
-#     assert response.status_code == 200   
+def test_post_diet():
+    diet =   data[0].get("diet")
+    response = requests.post(BASE_URL + "/dietPlan" , json =diet ) 
+    assert response.status_code == 200   
 
 
-# def test_put_note():
-#     data  = {
-#         "note" : "This note is to be updated"
-#     }
-#     response = requests.put(BASE_URL + "/note/1" , json=data )
-#     assert response.status_code == 200
+def test_put_note():
+    note  = data[0].get("note")
+    response = requests.put(BASE_URL + "/note/1" , json=note )
+    assert response.status_code == 200
 
 
 
